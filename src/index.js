a287e32a4a11c91442f92d9f9407bfb32e1f6f2c
safkat07 <safkatsadli@gmail.com>
2024-08-08 19:41:02 +0600

    custom error handler created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 
 dotenv.config({
@@ -7,6 +8,20 @@ dotenv.config({
 })
 
 connectDB()
+    .then(() => {
+
+        app.on("error", (error) => {
+            console.log("Error Before listen: ", error); 
+        })
+
+        app.listen((process.env.PORT || 8000), () => {
+            console.log(`Server is listening on ${process.env.PORT}`); 
+        })
+    })
+
+    .catch((err) => {
+        console.log("MOGODB connection error!!!", err);
+    })
 
 
 
